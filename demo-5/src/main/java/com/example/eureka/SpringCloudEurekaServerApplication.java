092f32407ccdc419f37f34e14b0dc03b0f5b7a10
bhps69 <33959580+bhps69@users.noreply.github.com>
2021-04-06 19:54:36 +0530

    serverdir

---
+++ demo-5/src/main/java/com/example/eureka/SpringCloudEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudEurekaServerApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+	}
+
+}
